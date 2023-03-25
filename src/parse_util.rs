9afe4a1c1b34fb474ddb9bb1d868ed70dce24e97
ttboma <ttboma@gmail.com>
2023-03-25 22:10:37 +0800

    (Parse) Add utitily to parse 2D list

--- src/parse_util.rs
+++ src/parse_util.rs
@@ -1,13 +1,5 @@
 use std::io;
-use std::io::prelude::*;
 
-#[allow(dead_code)]
-fn read_string() -> io::Result<String> {
-    let mut stdin = io::stdin();
-    let mut buffer = String::new();
-    stdin.read_to_string(&mut buffer)?;
-    Ok(buffer)
-}
 pub fn read_line() -> io::Result<String> {
     let stdin = io::stdin();
     let mut buffer = String::new();
@@ -16,9 +8,13 @@ pub fn read_line() -> io::Result<String> {
 }
 
 use nom::{
-    bytes::complete::tag, bytes::complete::take_while1, character::complete::multispace0,
-    combinator::opt, error::ParseError, multi::many1, multi::separated_list0, sequence::delimited,
-    sequence::preceded, sequence::terminated, IResult,
+    bytes::complete::{tag, take_while1},
+    character::complete::{multispace0, digit1},
+    combinator::opt,
+    error::ParseError,
+    multi::separated_list0,
+    sequence::{delimited, preceded, terminated, tuple},
+    IResult,
 };
 fn ws<'a, F: 'a, O, E: ParseError<&'a str>>(
     inner: F,
@@ -41,31 +37,19 @@ pub fn parse_list(input: &str) -> IResult<&str, Vec<&str>> {
         ws(tag("]")),
     )(input)
 }
-pub fn parse_lists(input: &str) -> IResult<&str, Vec<Vec<&str>>> {
-    many1(delimited(
+pub fn parse_list_2d(input: &str) -> IResult<&str, Vec<Vec<&str>>> {
+    delimited(
         ws(tag("[")),
-        terminated(
-            separated_list0(
-                ws(tag(",")),
-                take_while1(|x: char| x.is_ascii() && x != ',' && x != ']' && !x.is_whitespace()),
-            ),
-            opt(ws(tag(","))),
-        ),
+        terminated(separated_list0(ws(tag(",")), parse_list), opt(ws(tag(",")))),
         ws(tag("]")),
-    ))(input)
+    )(input)
+}
+pub fn parse_i32_and_list_2d(input: &str) -> IResult<&str, (&str, Vec<Vec<&str>>)> {
+    tuple((delimited(multispace0, digit1, multispace0), parse_list_2d))(input)
 }
 pub fn parse_string(input: &str) -> IResult<&str, &str> {
     preceded(
         multispace0,
         take_while1(|x: char| x.is_ascii() && !x.is_whitespace()),
     )(input)
-}
-pub fn read_i32() -> i32 {
-    let buffer = read_line().unwrap();
-    buffer.trim().parse().unwrap()
-}
-pub fn read_i32_list() -> Vec<i32> {
-    let buffer = read_line().unwrap();
-    let (_, list) = parse_list(&buffer).unwrap();
-    list.iter().map(|s| s.parse().unwrap()).collect()
 }
