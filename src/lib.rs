5b437f40a843fc864158de1b072c6d633632ab17
ttboma <ttboma@gmail.com>
2023-04-16 21:44:13 +0800

    (Solution) 338. Counting Bits

--- src/lib.rs
+++ src/lib.rs
@@ -19,9 +19,13 @@ impl ListNode {
 /// LeetCode solutions provided by Shieh, Yueh-chang <ttboma@gmail.com>
 pub struct Solution {}
 
+/// LeetCode solutions provided by Shieh, Yueh-chang <ttboma@gmail.com>
 /// Each of the methods for Solution struct are placed under its own module
+pub struct Solution {}
+mod count_bits;
 mod fib;
-mod total_fruit;
+mod find_duplicate_subtrees;
+mod find_kth_positive;
 mod generate;
 mod is_isomorphic;
 mod is_subsequence;
@@ -30,10 +34,12 @@ mod longest_palindrome;
 mod max_profit;
 mod merge_two_lists;
 mod middle_node;
+mod min_eating_speed;
+mod min_score;
 mod pivot_index;
 mod reverse_list;
 mod running_sum;
-mod count_odds;
+mod search;
 mod ship_within_days;
 mod find_kth_positive;
 mod min_eating_speed;
