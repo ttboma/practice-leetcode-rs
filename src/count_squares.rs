f013b707dd4b1c6a03f63e97ff90cc17350c5549
ttboma <ttboma@gmail.com>
2023-09-25 18:03:57 +0800

    fix: doc

--- src/count_squares.rs
+++ src/count_squares.rs
@@ -7,7 +7,7 @@ impl Solution {
     ///
     /// **Example 1:**
     ///
-    /// ```
+    /// ```txt
     /// Input: matrix =
     /// [
     ///  [0,1,1,1],
@@ -24,7 +24,7 @@ impl Solution {
     ///
     /// **Example 2:**
     ///
-    /// ```
+    /// ```txt
     /// Input: matrix =
     /// [
     ///   [1,0,1],
