f3503c6ff457479a7df5533b7b301ca6137b09ba
ttboma <ttboma@gmail.com>
2023-11-08 15:46:22 +0800

    refactor: 121. Best Time to Buy and Sell Stock

--- src/max_profit.rs
+++ src/max_profit.rs
@@ -1,58 +1,43 @@
 use crate::Solution;
 
 impl Solution {
-    /// # 121. Best Time to Buy and Sell Stock
+    /// # [121. Best Time to Buy and Sell Stock](https://leetcode.com/problems/best-time-to-buy-and-sell-stock/?envType=study-plan-v2&envId=top-interview-150)
     ///
-    /// You are given an array prices where prices[i] is the price of
-    /// a given stock on the ith day. You want to maximize your profit
-    /// by choosing a single day to buy one stock and choosing a
-    /// different day in the future to sell that stock. Return the
-    /// maximum profit you can achieve from this transaction. If you
-    /// cannot achieve any profit, return 0.
+    /// You are given an array `prices` where `prices[i]` is the price of a given stock on the `i^th` day.
+    ///
+    /// You want to maximize your profit by choosing a **single day**  to buy one stock and choosing a **different day in the future**  to sell that stock.
+    ///
+    /// Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return `0`.
     ///
     /// **Example 1:**
     ///
-    /// - **Input:** prices = [7,1,5,3,6,4]
-    /// - **Output:** 5
-    /// - **Explanation:** Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
-    ///   Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+    /// ```txt
+    /// Input: prices = [7,1,5,3,6,4]
+    /// Output: 5
+    /// Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+    /// Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+    /// ```
     ///
     /// **Example 2:**
     ///
-    /// - **Input:** prices = [7,6,4,3,1]
-    /// - **Output:** 0
-    /// - **Explanation:** In this case, no transactions are done and the max profit = 0.
-    ///
+    /// ```txt
+    /// Input: prices = [7,6,4,3,1]
+    /// Output: 0
+    /// Explanation: In this case, no transactions are done and the max profit = 0.
+    /// ```
     ///
     /// **Constraints:**
     ///
-    /// - 1 <= prices.length <= 105
-    /// - 0 <= prices[i] <= 104
-    ///
-    /// ------
-    ///
-    /// ***Extracted from:*** [best-time-to-buy-and-sell-stock](https://leetcode.com/problems/best-time-to-buy-and-sell-stock/)
+    /// - `1 <= prices.length <= 10^5`
+    /// - `0 <= prices[i] <= 10^4`
     pub fn max_profit(prices: Vec<i32>) -> i32 {
-        let mut max = 0;
-        let mut buy = prices[0];
-        let mut sell = prices[0];
-
-        for i in prices.iter().skip(1) {
-            if *i < buy {
-                let profit = sell - buy;
-                max = if profit > max { profit } else { max };
-                sell = *i;
-                buy = *i;
-            } else if *i > sell {
-                sell = *i;
-            }
+        let mut min_price = prices[0];
+        let mut max_profit = 0;
+        for price in prices.iter().skip(1) {
+            min_price = min_price.min(*price);
+            max_profit = max_profit.max(price - min_price);
         }
-        let profit = sell - buy;
-        if max < profit {
-            profit
-        } else {
-            max
-        }
+        max_profit
     }
 }
 
