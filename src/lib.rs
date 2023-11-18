6efac0b1baaaffcc190dca9614342babcce8b80c
ttboma <ttboma@gmail.com>
2023-11-18 11:03:14 +0800

    refactor: readme and doc

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,17 @@
 //! The purpose of this project is to practice Rust and DSA.
 //!
 //! 1. Please run `cargo test` to ensure that all the tests are passed correctly
-//! 2. Please run `cargo doc --open` to access the documentation for this crate
-//! 3. To access the command line interface for this crate, please run
+//! 2. The following shows an example that how to use this library crate, to access
+//!    the documentation, please run `cargo doc --open`
+//!
+//!    ```rust
+//!    use leetcode_rs::Solution;
+//!
+//!    let ans = Solution::fib(5);
+//!    assert_eq!(ans, 5);
+//!    ```
+//!
+//! 3. To access the command line interface, please run
 //!
 //!    ```bash
 //!    cargo run --bin solution -- --help
