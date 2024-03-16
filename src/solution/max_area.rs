d30269da77325865b31a8b355c1183af2e9a8a94
ttboma <ttboma@gmail.com>
2024-03-16 17:20:24 +0800

    fix: doc

--- src/solution/max_area.rs
+++ src/solution/max_area.rs
@@ -15,7 +15,7 @@ impl Solution {
     /// **Example 1:**
     /// <img alt="" src="https://s3-lc-upload.s3.amazonaws.com/uploads/2018/07/17/question_11.jpg" style="width: 600px; height: 287px;">
     ///
-    /// ```
+    /// ```txt
     /// Input: height = [1,8,6,2,5,4,8,3,7]
     /// Output: 49
     /// Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
@@ -23,7 +23,7 @@ impl Solution {
     ///
     /// **Example 2:**
     ///
-    /// ```
+    /// ```txt
     /// Input: height = [1,1]
     /// Output: 1
     /// ```
