c0d30c03ab12f0fb8a535d0c7211d16f7ccd09a2
ttboma <ttboma@gmail.com>
2024-08-23 08:54:10 -0400

    refactor: docs

--- src/solution/count_odds.rs
+++ src/solution/count_odds.rs
@@ -1,25 +1,23 @@
 use crate::Solution;
 
 impl Solution {
-    /// # [1523. Count Odd Numbers in an Interval Range](https://leetcode.com/problems/count-odd-numbers-in-an-interval-range/)
+    /// # [1523. Count Odd Numbers in an Interval Range](https://leetcode.com/problems/count-odd-numbers-in-an-interval-range/description/)
     ///
-    /// Given two non-negative integers `low` and `<font face="monospace">high</font>`. Return the count of odd numbers between `low` and `<font face="monospace">high</font>`(inclusive).
+    /// Given two non-negative integers `low` and `high`. Return the count of odd numbers between `low` and `high`(inclusive).
     ///
     /// **Example 1:**
     ///
     /// ```txt
     /// Input: low = 3, high = 7
     /// Output: 3
-    /// <b>Explanation: </b>The odd numbers between 3 and 7 are [3,5,7].
-    /// ```
+    /// <b>Explanation: </b>The odd numbers between 3 and 7 are [3,5,7].```
     ///
     /// **Example 2:**
     ///
     /// ```txt
     /// Input: low = 8, high = 10
     /// Output: 1
-    /// <b>Explanation: </b>The odd numbers between 8 and 10 are [9].
-    /// ```
+    /// <b>Explanation: </b>The odd numbers between 8 and 10 are [9].```
     ///
     /// **Constraints:**
     ///
