761e185ffe6ac524178fd6c4b456f8f0570db115
johns575 <johns575@andestech.com>
2024-11-26 18:47:32 +0800

    fix: doc

--- src/solution/reverse_bits.rs
+++ src/solution/reverse_bits.rs
@@ -12,7 +12,7 @@ impl Solution {
     ///
     /// **Example 1:**
     ///
-    /// ```
+    /// ```txt
     /// Input: n = 00000010100101000001111010011100
     /// Output:    964176192 (00111001011110000010100101000000)
     /// Explanation: The input binary string **00000010100101000001111010011100**  represents the unsigned integer 43261596, so return 964176192 which its binary representation is **00111001011110000010100101000000** .
@@ -20,7 +20,7 @@ impl Solution {
     ///
     /// **Example 2:**
     ///
-    /// ```
+    /// ```txt
     /// Input: n = 11111111111111111111111111111101
     /// Output:   3221225471 (10111111111111111111111111111111)
     /// Explanation: The input binary string **11111111111111111111111111111101**  represents the unsigned integer 4294967293, so return 3221225471 which its binary representation is **10111111111111111111111111111111** .
